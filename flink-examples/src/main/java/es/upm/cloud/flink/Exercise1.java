d5576b23fb30f520acd8688e26206267c679dae4
Gabriela Argüelles Terrón <gabyarte2@gmail.com>
2022-11-24 13:50:05 +0100

    feat: Create project

---
+++ flink-examples/src/main/java/es/upm/cloud/flink/Exercise1.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package es.upm.cloud.flink;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.core.fs.FileSystem;
+
+/**
+ * Skeleton for a Flink DataStream Job.
+ *
+ * <p>For a tutorial how to write a Flink application, check the
+ * tutorials and examples on the <a href="https://flink.apache.org">Flink Website</a>.
+ *
+ * <p>To package your application into a JAR file for execution, run
+ * 'mvn clean package' on the command line.
+ *
+ * <p>If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+public class Exercise1 {
+
+	public static void main(String[] args) throws Exception {
+		final ParameterTool params = ParameterTool.fromArgs(args);
+
+		// Sets up the execution environment, which is the main entry point
+		// to building Flink applications.
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+		/*
+		 * Here, you can start creating your execution plan for Flink.
+		 *
+		 * Start with getting some data from the environment, like
+		 * 	env.fromSequence(1, 10);
+		 *
+		 * then, transform the resulting DataStream<Long> using operations
+		 * like
+		 * 	.filter()
+		 * 	.flatMap()
+		 * 	.window()
+		 * 	.process()
+		 *
+		 * and many more.
+		 * Have a look at the programming guide:
+		 *
+		 * https://nightlies.apache.org/flink/flink-docs-stable/
+		 *
+		 */
+
+		DataStream<String> dataStream = env.readTextFile(params.get("input"));
+
+		if (params.has("output")) {
+			dataStream.writeAsText(params.get("output"), FileSystem.WriteMode.OVERWRITE);
+		}
+        else {
+            System.out.println("Printing result to stdout. Use --output to specify output path");
+            dataStream.print();
+        }
+
+
+		// Execute program, beginning computation.
+		env.execute("SourceSink");
+	}
+}
